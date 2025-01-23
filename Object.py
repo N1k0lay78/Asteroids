8a3e97ee9e61211e912e00905db15b54debfe7eb
N1k0lay78 <49759787+N1k0lay78@users.noreply.github.com>
2025-01-23 18:18:02 +0300

    base

---
+++ Object.py
@@ -0,0 +1,4 @@
+class Object:
+    def __init__(self, position, velocity):
+        self.pos = position
+        self.vel = velocity
---
+++ Space.py
@@ -0,0 +1,13 @@
+class Space:
+    def __init__(self):
+        self.objects = [
+
+        ]
+        self.world_size = (600, 600)
+        self.dt = 1/30
+    
+    def update_object(self, obj):
+        pass
+
+    def update(self):
+        pass

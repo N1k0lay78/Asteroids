c8572090d909078443942401bee877de29ff59cb
Robotop0 <meatyeah2110@gmail.com>
2025-01-23 19:00:16 +0300

    ARCHEDONS FIRE

--- Object.py
+++ Object.py
@@ -2,3 +2,12 @@ class Object:
     def __init__(self, position, velocity):
         self.pos = position
         self.vel = velocity
+
+    def get_pos(self):
+        return self.pos
+    
+    def get_vel(self):
+        return self.vel
+    
+    def set_pos(self, new_pos):
+        self.pos = new_pos
--- Space.py
+++ Space.py
@@ -1,13 +1,27 @@
+from Object import Object
+
 class Space:
     def __init__(self):
         self.objects = [
-
+            Object([300, 300], [35, 35])
         ]
         self.world_size = (600, 600)
         self.dt = 1/30
     
-    def update_object(self, obj):
-        pass
+    def update_object(self, obj: Object):
+        pos = obj.get_pos()
+        vel = obj.get_vel()
+        pos[0] += vel[0] * self.dt
+        pos[1] += vel[1] * self.dt
+        obj.set_pos(new_pos=pos)
+
 
     def update(self):
-        pass
+        for obj in self.objects:
+            self.update_object(obj)
+
+
+game = Space()
+for i in range(20):
+    game.update()
+    print(game.objects[0].get_pos())

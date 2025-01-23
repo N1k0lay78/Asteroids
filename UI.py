80135a888238c1bd05272da2a85264c80161e3df
N1k0lay78 <49759787+N1k0lay78@users.noreply.github.com>
2025-01-23 19:10:12 +0300

    add skelet for ui

---
+++ UI.py
@@ -0,0 +1,27 @@
+from Space import Space
+from Object import Object
+import pygame
+from random import randrange
+
+
+class GameUI:
+    def __init__(self):
+        self.space = Space()
+        
+        # инициализируем пайгейм
+        # загружаем картинку астеройда
+
+    def input(self):
+        # обработка нажатий
+        pass
+    
+    def draw_world(self):
+        # отрисовка объектов
+        pass
+    
+    def run(self):
+        # игровой цикл
+        pass
+
+
+GameUI().run()

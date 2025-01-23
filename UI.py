11524e6030c8fac0e48d32079f271cfcff78fe29
Robotop0 <meatyeah2110@gmail.com>
2025-01-23 19:30:54 +0300

    TMP

--- UI.py
+++ UI.py
@@ -7,21 +7,35 @@ from random import randrange
 class GameUI:
     def __init__(self):
         self.space = Space()
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.space.world_size))
+        self.screen.fill((255, 255, 255))
+        pygame.display.set_caption('SAMOKATSTEROIDS')
+        self.asteroid_sprite = pygame.image.load('Samokat.png')
+        self.asteroid_sprite.set_colorkey((255,255,255))
         
-        # инициализируем пайгейм
         # загружаем картинку астеройда
 
     def input(self):
-        # обработка нажатий
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
     
     def draw_world(self):
-        # отрисовка объектов
-        pass
+        for obj in self.space.objects:
+            self.screen.blit(self.asteroid_sprite, obj.get_pos())
     
     def run(self):
-        # игровой цикл
-        pass
+        while True:
+            self.input()
 
+            self.space.update()
+
+            self.draw_world()
+
+            pygame.display.update()
+            self.clock.tick(30)
 
 GameUI().run()
